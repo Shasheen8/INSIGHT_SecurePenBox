34d9094d69944bf028f47677f72363cc68fadde9
Shasheen_B <50681308+Shasheen8@users.noreply.github.com>
2020-06-13 01:41:48 -0400

    Add files via upload

---
+++ upload_dir.py
@@ -0,0 +1,60 @@
+import boto
+import boto.s3
+
+import os.path
+import sys
+
+# Fill these in - you get them when you sign up for S3
+AWS_ACCESS_KEY_ID = 'AKIAXLC2QA4VHAFOHRGC'
+AWS_ACCESS_KEY_SECRET = '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'
+# Fill in info on data to upload
+# destination bucket name
+bucket_name = 'webappsimages'
+# source directory
+sourceDir = 'D:\Docker_Test'
+# destination directory name (on s3)
+destDir = 'webappsimages'
+
+#max size in bytes before uploading in parts. between 1 and 5 GB recommended
+MAX_SIZE = 20 * 1000 * 1000
+#size of parts when uploading in parts
+PART_SIZE = 6 * 1000 * 1000
+
+conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_ACCESS_KEY_SECRET)
+
+bucket = conn.get_bucket(bucket_name)
+
+uploadFileNames = []
+for (sourceDir, dirname, filename) in os.walk(sourceDir):
+    uploadFileNames.extend(filename)
+    break
+
+def percent_cb(complete, total):
+    sys.stdout.write('.')
+    sys.stdout.flush()
+
+for filename in uploadFileNames:
+    sourcepath = os.path.join(sourceDir + '/' + filename)
+    destpath = os.path.join(destDir, filename)
+    print ('Uploading %s to Amazon S3 bucket %s' % \
+        (sourcepath, bucket_name))
+
+    filesize = os.path.getsize(sourcepath)
+    if filesize > MAX_SIZE:
+        print ("multipart upload")
+        mp = bucket.initiate_multipart_upload(destpath)
+        fp = open(sourcepath,'rb')
+        fp_num = 0
+        while (fp.tell() < filesize):
+            fp_num += 1
+            print ("uploading part %i" %fp_num)
+            mp.upload_part_from_file(fp, fp_num, cb=percent_cb, num_cb=10, size=PART_SIZE)
+
+        mp.complete_upload()
+
+    else:
+        print ("singlepart upload")
+        k = boto.s3.key.Key(bucket)
+        k.key = destpath
+        k.set_contents_from_filename(sourcepath,
+                cb=percent_cb, num_cb=10)
